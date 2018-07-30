127c4f16215d45ea0df1d64581c94cbc99325668
PhuongTran <tranduyphuong2110@gmail.com>
2018-07-29 21:11:39 -0700

    initial commit

---
+++ AsyncDemo/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(Object sender, EventArgs e)
+        {
+            Thread.Sleep(10000);
+            MessageBox.Show("hello");
+        }
+
+        private async void Button_ClickedAsync( Object sender, EventArgs e)
+        {
+            await Task.Delay(10000);
+            MessageBox.Show("hello async");
+        }
+    }
+}
